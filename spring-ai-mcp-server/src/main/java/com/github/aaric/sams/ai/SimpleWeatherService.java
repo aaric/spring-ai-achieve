71933fa9d2f4e0aa818a3738179259eb4be92617
qlhan <qlhan@incarcloud.com>
2025-05-09 17:19:07 +0800

    feat: mcp server - cityCode

---
+++ spring-ai-mcp-server/src/main/java/com/github/aaric/sams/ai/SimpleWeatherService.java
@@ -0,0 +1,15 @@
+package com.github.aaric.sams.ai;
+
+/**
+ * SimpleWeatherService
+ *
+ * @author Aaric
+ * @version 0.4.0-SNAPSHOT
+ */
+
+public interface SimpleWeatherService {
+
+    String cityCode(String cityName);
+
+//    String cityWeather(String cityCode);
+}
---
+++ spring-ai-mcp-server/src/main/java/com/github/aaric/sams/config/RestTemplateConfig.java
@@ -0,0 +1,30 @@
+package com.github.aaric.sams.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplateConfig
+ *
+ * @author Aaric
+ * @version 0.4.0-SNAPSHOT
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(150000);
+        factory.setConnectTimeout(150000);
+        return factory;
+    }
+}

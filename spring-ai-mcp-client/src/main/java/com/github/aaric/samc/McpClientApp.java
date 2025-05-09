c0d3d9e2f6d0045b0f7326d9a9e0835913e3462a
qlhan <qlhan@incarcloud.com>
2025-05-09 11:04:19 +0800

    feat: mcp client - init

---
+++ spring-ai-mcp-client/src/main/java/com/github/aaric/samc/McpClientApp.java
@@ -0,0 +1,18 @@
+package com.github.aaric.samc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * McpClientApp
+ *
+ * @author Aaric
+ * @version 0.3.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class McpClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpClientApp.class, args);
+    }
+}

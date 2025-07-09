19f602afb248bce0c94db50f18ecfd168bf49ee9
qlhan <qlhan@incarcloud.com>
2025-07-09 17:19:18 +0800

    test: some gen

--- spring-ai-api-client/src/test/java/com/github/aaric/saac/OpenAiApiTests.java
+++ spring-ai-api-client/src/test/java/com/github/aaric/saac/OpenAiApiTests.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * OpenAiApiTests
  *
@@ -29,5 +32,22 @@ public class OpenAiApiTests {
 //        System.err.println(chatClient.prompt().user("根据武汉的天气情况，请给岀行建议？").call().content());
 //        System.err.println(chatClient.prompt().user("根据今天武汉的天气情况，请给岀我的岀行建议？").call().content());
         System.err.println(chatClient.prompt().user("今天武汉是否适合外出活动？").call().content());
+    }
+
+    @Test
+    public void testGenTankVoltSQL() {
+        String data = """
+                TODO
+                """;
+        int typeId = 8;
+        System.err.printf("DELETE FROM t_par*t*ner_res*o*urce WHERE par*t*ner_type_id = %d;\n\n", typeId);
+        System.err.println("INSERT INTO t_par*t*ner_res*o*urce (pr_id, par*t*ner_type_id, res*o*urce_id) VALUES");
+
+        List<String> idList = Arrays.stream(data.split("\n"))
+                .filter(s -> !s.isEmpty())
+                .toList();
+        for (int i = 0; i < idList.size(); i++) {
+            System.err.printf("(%d, %d, %s),\n", (2000 + typeId * 100 + i), typeId, idList.get(i));
+        }
     }
 }

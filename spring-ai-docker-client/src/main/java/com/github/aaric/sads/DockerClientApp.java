0f11fda28ff4de459facb44d585c070058f65da5
qlhan <qlhan@incarcloud.com>
2025-07-25 16:54:50 +0800

    test: init new sub

---
+++ spring-ai-docker-client/src/main/java/com/github/aaric/sads/DockerClientApp.java
@@ -0,0 +1,18 @@
+package com.github.aaric.sads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DockerClientApp
+ *
+ * @author Aaric
+ * @version 0.11.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class DockerClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerClientApp.class, args);
+    }
+}

285e0d1a5d84fdffa85b05154998716120f51901
qlhan <qlhan@incarcloud.com>
2025-07-29 17:33:42 +0800

    test: dify client

---
+++ spring-ai-dify-client/src/main/java/com/github/aaric/sadc/config/DifyProperties.java
@@ -0,0 +1,23 @@
+package com.github.aaric.sadc.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * DifyProperties
+ *
+ * @author Aaric
+ * @version 0.6.0-SNAPSHOT
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "dify")
+public class DifyProperties {
+
+    private String baseUrl;
+
+    private Map<String, String> appKey;
+}
--- spring-ai-dify-client/src/test/java/com/github/aaric/sadc/DifyClientTests.java
+++ spring-ai-dify-client/src/test/java/com/github/aaric/sadc/DifyClientTests.java
@@ -1,7 +1,14 @@
 package com.github.aaric.sadc;
 
+import com.github.aaric.sadc.config.DifyProperties;
+import io.github.imfangs.dify.client.DifyChatflowClient;
+import io.github.imfangs.dify.client.DifyClientFactory;
+import io.github.imfangs.dify.client.enums.ResponseMode;
+import io.github.imfangs.dify.client.model.chat.ChatMessage;
+import io.github.imfangs.dify.client.model.chat.ChatMessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * DifyClientTests
  *
  * @author Aaric
- * @version 0.5.0-SNAPSHOT
+ * @version 0.14.0-SNAPSHOT
  */
 @Slf4j
 @SpringBootTest
@@ -20,8 +27,24 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class DifyClientTests {
 
+    private final DifyProperties difyProperties;
+
+    @Disabled
     @Test
-    public void testPrint() {
-        System.err.println("Hello World!");
+    public void testWorkflow() throws Exception {
+        log.debug("difyProperties: {}", difyProperties);
+        DifyChatflowClient chatFlowClient = DifyClientFactory.createChatWorkflowClient(difyProperties.getBaseUrl(),
+                difyProperties.getAppKey().get("ai-joke-campus"));
+
+        ChatMessage message = ChatMessage.builder()
+                .query("你好，请介绍一下自己")
+                .user("user-123")
+                .responseMode(ResponseMode.BLOCKING)
+                .build();
+
+        ChatMessageResponse response = chatFlowClient.sendChatMessage(message);
+        log.debug("conversationId={}", response.getConversationId());
+        log.debug("answer={}", response.getAnswer());
+        log.debug("messageId={}", response.getMessageId());
     }
 }

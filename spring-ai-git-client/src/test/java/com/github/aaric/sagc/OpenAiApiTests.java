879d27d53392adc0eaaa11c52f0aa7080f9b203c
qlhan <qlhan@incarcloud.com>
2025-07-31 16:23:27 +0800

    test: custom system prompt

--- spring-ai-git-client/src/test/java/com/github/aaric/sagc/OpenAiApiTests.java
+++ spring-ai-git-client/src/test/java/com/github/aaric/sagc/OpenAiApiTests.java
@@ -23,8 +23,19 @@ public class OpenAiApiTests {
 
     private final ChatClient chatClient;
 
+    private final ChatClient.Builder builder;
+
     @Test
     public void testPromptText() {
         System.err.println(chatClient.prompt().user("Git最新版本是啥？").call().content());
+    }
+
+    @Test
+    public void testPromptText2() {
+        ChatClient chatClient2 = builder.defaultSystem("""
+                        你是一个经验丰富的Java程序员，能够识别和分析代码，并按照要求给代码添加合理的注释。
+                        """)
+                .build();
+        System.err.println(chatClient2.prompt().user("Git最新版本是啥？").call().content());
     }
 }
